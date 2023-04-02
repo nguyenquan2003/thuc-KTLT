c408b73387459d6472f513bc369479ffa6aec3bd
Quân <127272332+nguyenquan2003@users.noreply.github.com>
2023-04-02 09:23:55 +0700

    Add files via upload

---
+++ ham xuat ra man hinh co dau tieng viet.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fcntl.h> //_O_WTEXT
+#include <io.h>    //_setmode()
+#include <string>
+using namespace std;
+int main()
+{
+    _setmode(_fileno(stdout), _O_WTEXT); // needed for output
+    _setmode(_fileno(stdin), _O_WTEXT);  // needed for input
+    wcout << L"Nhập tên của bạn: ";
+    wstring name;
+    getline(wcin, name);
+    wcout << L"Xin chào, " << name << L"\n";
+}
